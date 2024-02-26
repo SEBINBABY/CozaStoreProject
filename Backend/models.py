4106b779c71dba5ab3cf2e19fd6da97abd1a3107
SEBIN BABY <sebin.18@gmail.com>
2024-02-26 15:37:53 +0530

    second commit

--- Backend/models.py
+++ Backend/models.py
@@ -10,7 +10,7 @@ class CategoryDB(models.Model):
 class ProdcutDB(models.Model):
     CATEGORY = models.CharField(max_length=100, null=True, blank=True)
     PRONAME = models.CharField(max_length=100, null=True, blank=True)
-    PROPRICE = models.IntegerField(max_length=100, null=True, blank=True)
+    PROPRICE = models.IntegerField(null=True, blank=True)
     PRODESC = models.CharField(max_length=100, null=True, blank=True)
     PROIMAGE = models.ImageField(upload_to='Images', null=True, blank=True)
     PROIMAGE2 = models.ImageField(upload_to='Images', null=True, blank=True)
